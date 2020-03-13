0ab20f3bfa5c0a4f87a4d510f5f0407a1dd9a71d
mani140249 <mani140249@gmail.com>
2020-03-14 02:59:14 +0530

    Update settings.py

--- src/TechnoStart2020/settings.py
+++ src/TechnoStart2020/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e4f*#1)5&p=!@1s5s5=o&!krqi%)ur%fqe#_wbz^5v@zr*yzob'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.157','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
